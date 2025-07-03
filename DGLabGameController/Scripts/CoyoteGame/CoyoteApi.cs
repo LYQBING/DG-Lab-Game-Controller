65882b493704b86a98f19cf256cb6f88f27253b4
LYQBING <98566119+LYQBING@users.noreply.github.com>
2025-07-03 16:27:23 +0800

    更新DGLabCoyoteGameHub上游V2版本的API
    支持POST数据回执
    更新内置服务器启动事件

--- DGLabGameController/Scripts/CoyoteGame/CoyoteApi.cs
+++ DGLabGameController/Scripts/CoyoteGame/CoyoteApi.cs
@@ -1,194 +1,238 @@
 ﻿namespace lyqbing.DGLAB
 {
-    using System.Collections.Generic;
+	using System.Collections.Generic;
 
-    /// <summary>
-    /// 实现 DG-Lab-Coyote-Game-Hub API
-    /// </summary>
-    public sealed class CoyoteApi
-    {
-        private static readonly Lazy<CoyoteApi> InstanceLazy = new(() => new CoyoteApi());
-        private string _coyotreUrl = "http://127.0.0.1:8920/";
-        private const string ApiUrl = "api/v2/game/";
+	/// <summary>
+	/// 实现 DG-Lab-Coyote-Game-Hub API
+	/// </summary>
+	public sealed class CoyoteApi
+	{
+		private static readonly Lazy<CoyoteApi> InstanceLazy = new(() => new CoyoteApi());
+		private string _coyotreUrl = "http://127.0.0.1:8920/";
+		private const string ApiUrl = "api/v2/game/";
 
-        private CoyoteApi()
-        {
-        }
+		private CoyoteApi() { }
+		public static CoyoteApi Instance => InstanceLazy.Value;
 
-        public static CoyoteApi Instance => InstanceLazy.Value;
+		#region 配置服务器/本机属性
 
-        #region 配置服务器/本机属性
+		/// <summary>
+		/// Coyote-Game-Hub 服务器地址
+		/// </summary>
+		public static string CoyotreUrl
+		{
+			get => Instance._coyotreUrl;
+			set => Instance._coyotreUrl = "http://" + value;
+		}
 
-        /// <summary>
-        /// Coyote-Game-Hub 服务器地址
-        /// </summary>
-        public static string CoyotreUrl
-        {
-            get => Instance._coyotreUrl;
-            set => Instance._coyotreUrl = "http://" + value;
-        }
+		/// <summary>
+		/// 客户端ID
+		/// </summary>
+		public static string ClientID { get; set; } = "all";
 
-        /// <summary>
-        /// 客户端ID
-        /// </summary>
-        public static string ClientID { get; set; } = "all";
+		#endregion
 
-        #endregion
+		#region 获取对应功能 Api 地址
 
-        #region 获取对应功能 Api 地址
+		/// <summary>
+		/// 获取服务器响应数据 API
+		/// </summary>
+		public string ServerResponseApi => _coyotreUrl + ApiUrl;
 
-        /// <summary>
-        /// 一键开火 API
-        /// </summary>
-        public string FireApi => _coyotreUrl + ApiUrl + ClientID + "/action/fire";
+		/// <summary>
+		/// 获取游戏响应数据 API
+		/// </summary>
+		public string GameResponseApi => _coyotreUrl + ApiUrl + ClientID;
 
-        /// <summary>
-        /// 设置/读取 设置游戏强度配置 API
-        /// </summary>
-        public string StrengthConfigApi => _coyotreUrl + ApiUrl + ClientID + "/strength";
+		/// <summary>
+		/// 设置/读取 设置游戏强度配置 API
+		/// </summary>
+		public string StrengthApi => _coyotreUrl + ApiUrl + ClientID + "/strength";
 
-        /// <summary>
-        /// 获取获取服务器配置的波形列表 API
-        /// </summary>
-        public string PulseListApi => _coyotreUrl + "api/v2/pulse_list";
+		/// <summary>
+		/// 设置/获取 当前波形ID API
+		/// </summary>
+		public string PulseApi => _coyotreUrl + ApiUrl + ClientID + "/pulse";
 
-        /// <summary>
-        /// 获取完整的波形列表，包括客户端自定义波形 API
-        /// </summary>
-        public string AllPulseListApi => _coyotreUrl + ApiUrl + ClientID + "/pulse_list";
+		/// <summary>
+		/// 获取完整的波形列表，包括客户端自定义波形 API
+		/// </summary>
+		public string PulseListApi => _coyotreUrl + ApiUrl + ClientID + "/pulse_list";
 
-        /// <summary>
-        /// 设置/获取 当前波形ID API
-        /// </summary>
-        public string PulseIdApi => _coyotreUrl + ApiUrl + ClientID + "/pulse";
+		/// <summary>
+		/// 一键开火 API
+		/// </summary>
+		public string FireApi => _coyotreUrl + ApiUrl + ClientID + "/action/fire";
 
-        /// <summary>
-        /// 获取游戏响应数据 API
-        /// </summary>
-        public string GameResponseApi => _coyotreUrl + ApiUrl + ClientID;
+		#endregion
+	}
 
-        #endregion
-    }
+	#region 基础类型
 
-    #region 波形列表相关
+	/// <summary>
+	/// 警告信息
+	/// </summary>
+	public class Warnings
+	{
+		/// <summary>
+		/// 警告代码
+		/// </summary>
+		public string? Code { get; set; }
+		/// <summary>
+		/// 警告信息
+		/// </summary>
+		public string? Message { get; set; }
+	}
+	#endregion
 
-    /// <summary>
-    /// 波形列表回执JSON
-    /// </summary>
-    public class PulseListJson
-    {
-        public int status;
-        public string? code;
-        public List<PulseList>? pulseList;
-    }
+	#region 波形相关
 
-    /// <summary>
-    /// 波形列表配置文件
-    /// </summary>
-    public class PulseList
-    {
-        /// <summary>
-        /// 脉冲ID
-        /// </summary>
-        public string? id;
+	/// <summary>
+	/// 波形列表配置文件
+	/// </summary>
+	public class PulseList
+	{
+		/// <summary>
+		/// 脉冲ID
+		/// </summary>
+		public string? Id { get; set; }
 
-        /// <summary>
-        /// 脉冲名称
-        /// </summary>
-        public string? name;
-    }
+		/// <summary>
+		/// 脉冲名称
+		/// </summary>
+		public string? Name { get; set; }
+	}
 
-    #endregion
+	/// <summary>
+	/// 波形列表回执JSON
+	/// </summary>
+	public class PulseListJson
+	{
+		public int Status { get; set; }
+		public string? Code { get; set; }
+		public string? Message { get; set; }
+		public List<Warnings>? Warnings { get; set; }
+		public List<PulseList>? PulseList { get; set; }
+	}
 
-    #region 波形配置文件
+	/// <summary>
+	/// 波形配置文件
+	/// </summary>
+	public class PulseId
+	{
+		public int Status { get; set; }
+		public string? Code { get; set; }
 
-    /// <summary>
-    /// 波形配置文件
-    /// </summary>
-    public class PulseId
-    {
-        public int status;
-        public string? code;
+		/// <summary>
+		/// 当前波形ID
+		/// </summary>
+		public string? CurrentPulseId { get; set; }
 
-        /// <summary>
-        /// 当前波形ID
-        /// </summary>
-        public string? currentPulseId;
+		/// <summary>
+		/// 当前波形列表
+		/// </summary>
+		public string[]? PulseIdList { get; set; }
+	}
 
-        /// <summary>
-        /// 当前波形列表
-        /// </summary>
-        public string[]? pulseId;
-    }
+	#endregion
 
-    #endregion
+	#region 强度配置相关
 
-    #region 强度配置相关
+	/// <summary>
+	/// 强度配置文件
+	/// </summary>
+	public class StrengthConfig
+	{
+		/// <summary>
+		/// 基础强度
+		/// </summary>
+		public int Strength { get; set; }
 
-    /// <summary>
-    /// 强度配置回执JSON
-    /// </summary>
-    public class StrengthConfigJson
-    {
-        public int status;
-        public string? code;
-        public StrengthConfig? strengthConfig;
-    }
+		/// <summary>
+		/// 随机强度
+		/// </summary>
+		public int RandomStrength { get; set; }
+	}
 
-    /// <summary>
-    /// 强度配置文件
-    /// </summary>
-    public class StrengthConfig
-    {
-        /// <summary>
-        /// 基础强度
-        /// </summary>
-        public int strength;
+	/// <summary>
+	/// 强度配置回执JSON
+	/// </summary>
+	public class StrengthConfigJson
+	{
+		public int Status { get; set; }
+		public string? Code { get; set; }
+		public string? Message { get; set; }
+		public List<Warnings>? Warnings { get; set; }
+		public StrengthConfig? StrengthConfig { get; set; }
+	}
 
-        /// <summary>
-        /// 随机强度
-        /// </summary>
-        public int randomStrength;
-    }
+	/// <summary>
+	/// 服务器响应
+	/// </summary>
+	public class ServerResponse
+	{
+		public int Status { get; set; }
+		public string? Code { get; set; }
+		public string? Message { get; set; }
+		public List<Warnings>? Warnings { get; set; }
+		public int MinApiVersion { get; set; }
+		public int MaxApiVersion { get; set; }
+	}
 
-    #endregion
+	#endregion
 
-    #region 响应数据相关
+	#region 响应数据相关
 
-    /// <summary>
-    /// 当前配置数据
-    /// </summary>
-    public class GameConfig
-    {
-        public int[]? strengthChangeInterval;
-        public bool enableBChannel;
-        public float bChannelStrengthMultiplier;
-        public string? pulseId;
-        public string? pulseMode;
-        public int pulseChangeInterval;
-    }
+	/// <summary>
+	/// 当前配置数据
+	/// </summary>
+	public class GameConfig
+	{
+		public int? FireStrengthLimit { get; set; }
+		public int[]? StrengthChangeInterval { get; set; }
+		public bool EnableBChannel { get; set; }
+		public float BChannelStrengthMultiplier { get; set; }
+		public string? PulseId { get; set; }
+		public string? FirePulseId { get; set; }
+		public string? PulseMode { get; set; }
+		public int PulseChangeInterval { get; set; }
+	}
 
-    /// <summary>
-    /// 当前用户数据
-    /// </summary>
-    public class ClientStrength
-    {
-        public int strength;
-        public int limit;
-    }
+	/// <summary>
+	/// 当前用户数据
+	/// </summary>
+	public class ClientStrength
+	{
+		public int Strength { get; set; }
+		public int Limit { get; set; }
+	}
 
-    /// <summary>
-    /// 游戏响应数据回执
-    /// </summary>
-    public class GameResponse
-    {
-        public int status;
-        public string? code;
-        public StrengthConfig? strengthConfig;
-        public GameConfig? gameConfig;
-        public ClientStrength? clientStrength;
-        public string? currentPulseId;
-    }
+	/// <summary>
+	/// 游戏响应数据回执
+	/// </summary>
+	public class GameResponse
+	{
+		public int Status { get; set; }
+		public string? Code { get; set; }
+		public string? Message { get; set; }
+		public List<Warnings>? Warnings { get; set; }
+		public StrengthConfig? StrengthConfig { get; set; }
+		public GameConfig? GameConfig { get; set; }
+		public ClientStrength? ClientStrength { get; set; }
+		public string? CurrentPulseId { get; set; }
+	}
 
-    #endregion
+	/// <summary>
+	/// 一键开火响应
+	/// </summary>
+	public class FireJson
+	{
+		public int Status { get; set; }
+		public string? Code { get; set; }
+		public string? Message { get; set; }
+		public List<Warnings>? Warnings { get; set; }
+		public string[]? SuccessClientIds { get; set; }
+	}
+
+	#endregion
 }
--- DGLabGameController/Scripts/CoyoteGame/DGLab.cs
+++ DGLabGameController/Scripts/CoyoteGame/DGLab.cs
@@ -1,222 +1,162 @@
-﻿namespace lyqbing.DGLAB
+﻿using DGLabGameController;
+using Newtonsoft.Json;
+
+namespace lyqbing.DGLAB
 {
-    using Newtonsoft.Json.Linq;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
+	/// <summary>
+	/// DGLab 静态类，封装所有与游戏控制相关的 API 调用
+	/// </summary>
+	public static class DGLab
+	{
+		#region 通用私有方法
 
-    public static class DGLab
-    {
-        /// <summary>
-        /// 获取游戏响应数据
-        /// </summary>S
-        public static async Task<GameResponse?> GetGameResponse()
-        {
-            string? jsonPost = await FTPManager.GetAsync(CoyoteApi.Instance.GameResponseApi);
-            if (jsonPost is null)
-                return null;
+		/// <summary>
+		/// 通用 GET 请求并反序列化为指定类型
+		/// </summary>
+		private static async Task<T?> GetAndParseAsync<T>(string api)
+		{
+			string? json = await FTPManager.GetAsync(api);
+			return ParseJson<T>(json);
+		}
 
-            JToken token = JToken.Parse(jsonPost);
-            return token.ToObject<GameResponse>();
-        }
+		/// <summary>
+		/// 通用 POST 请求并反序列化为指定类型
+		/// </summary>
+		private static async Task<T?> PostAndParseAsync<T>(string api, IEnumerable<KeyValuePair<string, string>> formData)
+		{
+			string? json = await FTPManager.PostAsync(api, formData);
+			return ParseJson<T>(json);
+		}
 
-        #region 获取波形列表
+		/// <summary>
+		/// 通用 JSON 解析
+		/// </summary>
+		private static T? ParseJson<T>(string? json)
+		{
+			if (string.IsNullOrWhiteSpace(json)) return default;
+			try
+			{
+				return JsonConvert.DeserializeObject<T>(json);
+			}
+			catch (JsonException)
+			{
+				DebugHub.Error("解析失败", "无法解析服务器返回的 JSON 数据");
+				return default;
+			}
+		}
 
-        /// <summary>
-        /// 获取获取服务器配置的波形列表 API
-        /// </summary>
-        public static async Task<List<PulseList>?> GetPulseList()
-        {
-            string? jsonPost = await FTPManager.GetAsync(CoyoteApi.Instance.PulseListApi);
-            if (jsonPost is null)
-                return null;
+		#endregion
 
-            JToken token = JToken.Parse(jsonPost);
-            return token.ToObject<List<PulseList>>();
-        }
+		#region 响应数据
 
-        /// <summary>
-        /// 获取完整的波形列表，包括客户端自定义波形 API
-        /// </summary>
-        public static async Task<List<PulseList>?> GetAllPulseList()
-        {
-            string? jsonPost = await FTPManager.GetAsync(CoyoteApi.Instance.AllPulseListApi);
-            if (jsonPost is null)
-                return null;
+		/// <summary>
+		/// 获取服务器响应数据
+		/// </summary>
+		public static Task<ServerResponse?> GetServerResponse() => GetAndParseAsync<ServerResponse>(CoyoteApi.Instance.ServerResponseApi);
 
-            JToken token = JToken.Parse(jsonPost);
-            return token.ToObject<List<PulseList>>();
-        }
+		/// <summary>
+		/// 获取游戏响应数据
+		/// </summary>
+		public static Task<GameResponse?> GetGameResponse() => GetAndParseAsync<GameResponse>(CoyoteApi.Instance.GameResponseApi);
 
-        #endregion
+		#endregion
 
-        #region 一键开火
+		#region 波形相关
 
-        /// <summary>
-        /// 一键开火API
-        /// </summary>
-        /// <param name="strength">一键开火强度，最高40</param>
-        /// <param name="time">一键开火时间，单位：毫秒，默认为5000，最高30000（30秒）</param>
-        /// <param name="overrides">多次一键开火时，是否重置时间，true为重置时间，false为叠加时间，默认为false</param>
-        /// <param name="pulseId">一键开火的波形ID</param>
-        public static Task Fire(int strength, int time, bool overrides, string pulseId)
-        {
-            Dictionary<string, string> formData = new()
-            {
-                { "strength", strength.ToString() },
-                { "time", time.ToString() },
-                { "overrides", overrides.ToString() },
-                { "pulseId", pulseId }
-            };
-            return FTPManager.PostAsync(CoyoteApi.Instance.FireApi, formData);
-        }
+		/// <summary>
+		/// 获取波形列表
+		/// </summary>
+		public static Task<PulseListJson?> GetAllPulseList() => GetAndParseAsync<PulseListJson>(CoyoteApi.Instance.PulseListApi);
 
-        /// <summary>
-        /// 一键开火API
-        /// </summary>
-        /// <param name="strength">一键开火强度，最高40</param>
-        /// <param name="time">一键开火时间，单位：毫秒，默认为5000，最高30000（30秒）</param>
-        /// <param name="overrides">多次一键开火时，是否重置时间，true为重置时间，false为叠加时间，默认为false</param>
-        public static Task Fire(int strength = 20, int time = 5000, bool overrides = false)
-        {
-            Dictionary<string, string> formData = new()
-            {
-                { "strength", strength.ToString() },
-                { "time", time.ToString() },
-                { "overrides", overrides.ToString() }
-            };
-            return FTPManager.PostAsync(CoyoteApi.Instance.FireApi, formData);
-        }
+		/// <summary>
+		/// 获取游戏当前波形ID
+		/// </summary>
+		public static Task<PulseId?> GetPulseID() => GetAndParseAsync<PulseId>(CoyoteApi.Instance.PulseApi);
 
-        #endregion
+		/// <summary>
+		/// 设置游戏当前波形ID
+		/// </summary>
+		/// <param name="pulseId">波形ID</param>
+		public static Task<PulseId?> SetPulseID(string pulseId) => PostAndParseAsync<PulseId>(CoyoteApi.Instance.PulseApi,[new KeyValuePair<string, string>("pulseId", pulseId)]);
 
-        #region 获取游戏当前波形ID
+		/// <summary>
+		/// 设置游戏当前波形ID
+		/// </summary>
+		/// <param name="pulseIds">波形ID列表</param>
+		public static Task<PulseId?> SetPulseID(List<string> pulseIds) => PostAndParseAsync<PulseId>(CoyoteApi.Instance.PulseApi,pulseIds.Select(id => new KeyValuePair<string, string>("pulseId[]", id)));
 
-        /// <summary>
-        /// 获取游戏当前波形ID
-        /// 注意：与官方文档不同，文档中仅提示 pulseId (列表) 但还存在 currentPulseId (当前)；
-        /// 因此，若要获取当前波形ID：请获取 currentPulseId ；若 pulseId 为空，则仅为单一波形
-        /// </summary>
-        /// <returns></returns>
-        public static async Task<PulseId?> GetPulseID()
-        {
-            string? jsonPost = await FTPManager.GetAsync(CoyoteApi.Instance.PulseIdApi);
-            if (jsonPost is null)
-                return null;
+		#endregion
 
-            JToken token = JToken.Parse(jsonPost);
-            return token.ToObject<PulseId>();
-        }
+		#region 一键开火
 
-        #endregion
+		/// <summary>
+		/// 一键开火API
+		/// </summary>
+		/// <param name="strength">一键开火强度，最高40</param>
+		/// <param name="time">一键开火时间，单位：毫秒，默认为5000，最高30000（30秒）</param>
+		/// <param name="overrides">多次一键开火时，是否重置时间，true为重置时间，false为叠加时间，默认为false</param>
+		/// <param name="pulseId">一键开火的波形ID</param>
+		public static Task<FireJson?> Fire(int strength = 20, int time = 5000, bool overrides = false, string pulseId = "") =>
+		PostAndParseAsync<FireJson>(CoyoteApi.Instance.FireApi,
+			[
+				new KeyValuePair<string, string>("strength", strength.ToString()),
+				new KeyValuePair<string, string>("time", time.ToString()),
+				new KeyValuePair<string, string>("overrides", overrides.ToString()),
+				new KeyValuePair<string, string>("pulseId", pulseId)
+			]
+		);
 
-        #region 设置游戏当前波形ID
+		#endregion
 
-        /// <summary>
-        /// 设置游戏当前波形ID
-        /// </summary>
-        /// <param name="pulseId">波形ID</param>
-        public static Task SetPulseID(string pulseId)
-        {
-            Dictionary<string, string> formData = new()
-            {
-                { "pulseId", pulseId }
-            };
-            return FTPManager.PostAsync(CoyoteApi.Instance.PulseIdApi, formData);
-        }
+		#region 游戏强度配置相关
 
-        /// <summary>
-        /// 设置游戏当前波形List
-        /// </summary>
-        /// <param name="pulseIds">波形List</param>
-        public static Task SetPulseID(List<string> pulseIds)
-        {
-            var formData = pulseIds.Select(id => new KeyValuePair<string, string>("pulseId[]", id));
-            return FTPManager.PostAsync(CoyoteApi.Instance.PulseIdApi, formData);
-        }
+		/// <summary>
+		/// 设置基本游戏强度配置
+		/// </summary>
+		public static class SetStrength
+		{
+			/// <summary>
+			/// 增加强度
+			/// </summary>
+			public static Task<StrengthConfigJson?> Add(int value = 1) => PostAndParseAsync<StrengthConfigJson>(CoyoteApi.Instance.StrengthApi,[new KeyValuePair<string, string>("strength.add", value.ToString())]);
 
-        #endregion
+			/// <summary>
+			/// 减少强度
+			/// </summary>
+			public static Task<StrengthConfigJson?> Sub(int value = 1) => PostAndParseAsync<StrengthConfigJson>(CoyoteApi.Instance.StrengthApi,[new KeyValuePair<string, string>("strength.sub", value.ToString())]);
 
-        #region 游戏强度配置相关
+			/// <summary>
+			/// 设置强度
+			/// </summary>
+			public static Task<StrengthConfigJson?> Set(int value = 1) => PostAndParseAsync<StrengthConfigJson>(CoyoteApi.Instance.StrengthApi,[new KeyValuePair<string, string>("strength.set", value.ToString())]);
+		}
 
-        /// <summary>
-        /// 设置基本游戏强度配置
-        /// </summary>
-        public static class SetStrength
-        {
-            public static Task Add(int value = 1)
-            {
-                Dictionary<string, string> formData = new()
-                {
-                    { "strength.add", value.ToString() }
-                };
-                return FTPManager.PostAsync(CoyoteApi.Instance.StrengthConfigApi, formData);
-            }
+		/// <summary>
+		/// 设置随机游戏强度配置
+		/// </summary>
+		public static class SetRandomStrength
+		{
+			/// <summary>
+			/// 增加随机强度
+			/// </summary>
+			public static Task<StrengthConfigJson?> Add(int value = 1) => PostAndParseAsync<StrengthConfigJson>(CoyoteApi.Instance.StrengthApi,[new KeyValuePair<string, string>("randomStrength.add", value.ToString())]);
 
-            public static Task Sub(int value = 1)
-            {
-                Dictionary<string, string> formData = new()
-                {
-                    { "strength.sub", value.ToString() }
-                };
-                return FTPManager.PostAsync(CoyoteApi.Instance.StrengthConfigApi, formData);
-            }
+			/// <summary>
+			/// 减少随机强度
+			/// </summary>
+			public static Task<StrengthConfigJson?> Sub(int value = 1) => PostAndParseAsync<StrengthConfigJson>(CoyoteApi.Instance.StrengthApi,[new KeyValuePair<string, string>("randomStrength.sub", value.ToString())]);
 
-            public static Task Set(int value = 1)
-            {
-                Dictionary<string, string> formData = new()
-                {
-                    { "strength.set", value.ToString() }
-                };
-                return FTPManager.PostAsync(CoyoteApi.Instance.StrengthConfigApi, formData);
-            }
-        }
+			/// <summary>
+			/// 设置随机强度
+			/// </summary>
+			public static Task<StrengthConfigJson?> Set(int value = 1) => PostAndParseAsync<StrengthConfigJson>(CoyoteApi.Instance.StrengthApi,[new KeyValuePair<string, string>("randomStrength.set", value.ToString())]);
+		}
 
-        /// <summary>
-        /// 设置随机游戏强度配置
-        /// </summary>
-        public static class SetRandomStrength
-        {
-            public static Task Add(int value = 1)
-            {
-                Dictionary<string, string> formData = new()
-                {
-                    { "randomStrength.add", value.ToString() }
-                };
-                return FTPManager.PostAsync(CoyoteApi.Instance.StrengthConfigApi, formData);
-            }
+		/// <summary>
+		/// 获取游戏强度信息
+		/// </summary>
+		public static Task<StrengthConfigJson?> GetStrengthConfig() => GetAndParseAsync<StrengthConfigJson>(CoyoteApi.Instance.StrengthApi);
 
-            public static Task Sub(int value = 1)
-            {
-                Dictionary<string, string> formData = new()
-                {
-                    { "randomStrength.sub", value.ToString() }
-                };
-                return FTPManager.PostAsync(CoyoteApi.Instance.StrengthConfigApi, formData);
-            }
-
-            public static Task Set(int value = 1)
-            {
-                Dictionary<string, string> formData = new()
-                {
-                    { "randomStrength.set", value.ToString() }
-                };
-                return FTPManager.PostAsync(CoyoteApi.Instance.StrengthConfigApi, formData);
-            }
-        }
-
-        /// <summary>
-        /// 获取游戏强度信息
-        /// </summary>
-        public static async Task<StrengthConfigJson?> GetStrengthConfig()
-        {
-            string? jsonPost = await FTPManager.GetAsync(CoyoteApi.Instance.StrengthConfigApi);
-            if (jsonPost is null)
-                return null;
-
-            JToken token = JToken.Parse(jsonPost);
-            return token.ToObject<StrengthConfigJson>();
-        }
-
-        #endregion
-    }
+		#endregion
+	}
 }
