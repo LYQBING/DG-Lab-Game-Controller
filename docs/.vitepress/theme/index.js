26871fecfe92cbae5d3e3ce5c2d7218822e60989
LYQBING <98566119+LYQBING@users.noreply.github.com>
2025-09-11 20:41:47 +0800

    Delete docs directory

--- docs/.vitepress/theme/index.js
+++
@@ -1,6 +0,0 @@
-﻿import DefaultTheme from 'vitepress/theme'
-export default DefaultTheme
-
-import './styles/vars.css'
-import './styles/components/custom-block.css'
-import './styles/components/vp-home-hero.css'
